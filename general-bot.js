1df9943eeb3814a80bbea556221ca61e4965c16d
EC2 Default User <ec2-user@ip-172-31-54-194.ec2.internal>
2016-08-23 19:30:16 +0000

    Dropped music shit

--- general-bot.js
+++ general-bot.js
@@ -1,7 +1,5 @@
 var Discord = require("discord.js");
-var credentials = require("./auth.json");
 var settings = require("./settings.json");
-
 var client = new Discord.Client();
 
 var currentVoiceChannel = null;
@@ -9,38 +7,9 @@ client.on("debug", function(m) {console.log("[debug]", m)});
 client.on("warn", function(m){console.log("[warn]", m)});
 client.on("message", function(message) {
     
-    // Initialize music bot
-    if(message.content === "!music init"){
-        for(var curChan of message.channel.server.channels){
-            if(curChan instanceof Discord.VoiceChannel && curChan.name == settings.musicBotChannel){
-                client.reply(message, " you sure you can handle these beats?");
-                client.joinVoiceChannel(curChan);
-                currentVoiceChannel = curChan;
-                break;
-            }   
-        }
-    }
-    else if(message.content === "!music test"){
-        if (client.internal.voiceConnection) {
-			client.voiceConnection.playFile('./420.mp3', function(err, intent){
-                console.log(err);
-            });
-        }
-    }
-    else if(message.content.indexOf("!music play") == 0){
-        console.log(getDirectories(settings.musicPath + "/AFI"));
-    }
-    // Stop music playback
-    else if(message.content === "!music stop"){
-        if(client.internal.voiceConnection)
-            client.internal.voiceConnection.stopPlaying();
-    }
-    // Exit music bot
-    else if(message.content === "!music quit"){
-           exitVoiceChannel();
-    }
+    
     // Moves all members of the senders voice channel to the specified voice channel
-    else if(message.content.indexOf("!moveChannelTo") == 0){
+    if(message.content.indexOf("!moveChannelTo") == 0){
         // Get the channel name out of the command
         var dest = message.content.split(" ");
         dest = dest.slice(1,dest.length);
@@ -59,6 +28,10 @@ client.on("message", function(message) {
             }
         }
     }
+    else if (message.content === "!test"){
+	client.replyTTS(message,"It's working!");
+	client.reply(message, "https://giphy.com/gifs/9K2nFglCAQClO");
+    }
 });
 
 function exitVoiceChannel(){
@@ -68,4 +41,4 @@ function exitVoiceChannel(){
     }
 }
 
-client.login(credentials.email , credentials.password);
+client.login("" , "");
